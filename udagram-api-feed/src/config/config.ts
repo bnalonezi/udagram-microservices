f64d995b007d96a3e7675e941b0d4e35f56825d2
bnalonezi <bandar.alonezi@gmail.com>
2021-04-17 03:31:16 +0300

    Update URL

--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -7,7 +7,7 @@ export const config = {
   'aws_region': process.env.AWS_REGION,
   'aws_profile': process.env.AWS_PROFILE,
   'aws_media_bucket': process.env.AWS_BUCKET,
-  'url': 'http://localhost:8100',
+  'url': '*',
   'jwt': {
     'secret': process.env.JWT_SECRET,
   },
